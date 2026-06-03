6b1514ee3fbedbe20af14ea6c9d2ad1e7b6f998d
KarenTsaturyan <karentsaturyan2002@gmail.com>
2026-06-03 23:15:33 +0400

    09: Rate limitter

--- cmd/api/middleware.go
+++ cmd/api/middleware.go
@@ -2,21 +2,73 @@ package main
 
 import (
 	"fmt"
+	"net"
 	"net/http"
+	"sync"
+	"time"
 
 	"golang.org/x/time/rate"
 )
 
+// NOTE:
+// Using this pattern for rate-limiting will only work if your API application is running on a
+// single-machine. If your infrastructure is distributed, with your application running on
+// multiple servers behind a load balancer, then you’ll need to use an alternative approach
+// Can use REDIS, for saving client IP addresses and their rate limiters.
+// OR use Nginx
+
 func (app *application) rateLimit(next http.Handler) http.Handler {
-	// Initialize a new rate limiter which allows an average of 2 requests per second,
-	// with a maximum of 4 requests in a single ‘burst’.
-	limiter := rate.NewLimiter(2, 4)
+	type client struct {
+		limiter  *rate.Limiter
+		lastSeen time.Time
+	}
+	var (
+		mu sync.Mutex
+		// INFO: It hold the clients' IP addresses and rate limiters.
+		clients = make(map[string]*client)
+	)
+	// Launch a background goroutine which removes old entries from the clients map once
+	// every minute.
+	go func() {
+		for {
+			time.Sleep(time.Minute)
+
+			mu.Lock()
+			// INFO: Loop through all clients. If they haven't been seen within the last three
+			// minutes, delete the corresponding entry from the map.
+			for ip, client := range clients {
+				if time.Since(client.lastSeen) > 3*time.Minute {
+					delete(clients, ip)
+				}
+			}
+			mu.Unlock()
+		}
+	}()
 
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if !limiter.Allow() {
-			app.rateLimitExceededResponse(w, r)
-			return
+		if app.config.limiter.enabled {
+			ip, _, err := net.SplitHostPort(r.RemoteAddr)
+			if err != nil {
+				app.serverErrorResponse(w, r, err)
+				return
+			}
+			mu.Lock()
+
+			if _, found := clients[ip]; !found {
+				clients[ip] = &client{
+					limiter: rate.NewLimiter(rate.Limit(app.config.limiter.rps), app.config.limiter.burst),
+				}
+			}
+			clients[ip].lastSeen = time.Now()
+
+			if !clients[ip].limiter.Allow() {
+				mu.Unlock()
+				app.rateLimitExceededResponse(w, r)
+				return
+			}
+			mu.Unlock()
 		}
+
 		next.ServeHTTP(w, r)
 	})
 }
